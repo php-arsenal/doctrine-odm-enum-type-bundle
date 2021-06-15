a75c87319688018ac41bad19f7ac8c5bcd94c1bf
Gyvastis <shibalba1@gmail.com>
2021-06-15 12:33:01 +0200

    namespace update; add compiler pass

--- src/DoctrineODMEnumTypeBundle.php
+++ src/DoctrineODMEnumTypeBundle.php
@@ -2,8 +2,16 @@
 
 namespace PhpArsenal\DoctrineODMEnumTypeBundle;
 
+use PhpArsenal\DoctrineODMEnumTypeBundle\DoctrineExtensions\ManagerConfiguratorCompilerPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class DoctrineODMEnumTypeBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ManagerConfiguratorCompilerPass());
+    }
 }
